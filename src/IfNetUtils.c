0a443e1b1d45f278192e7e488bf9a9b1c3a16a96
Lukas Mazl <Lukas Mazl>
2020-10-14 16:09:32 +0200

    Removed useless things.....

--- src/IfNetUtils.c
+++
@@ -1,6 +0,0 @@
-//
-// Created by lukas on 14.10.20.
-//
-
-#include "IfNetUtils.h"
-
--- src/IfNetUtils.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by lukas on 14.10.20.
-//
-
-char* if_addr_family_to_string(int family);
-#ifndef PYTHON_TEST_MODULE_IFNETUTILS_H
-#define PYTHON_TEST_MODULE_IFNETUTILS_H
-#endif //PYTHON_TEST_MODULE_IFNETUTILS_H
