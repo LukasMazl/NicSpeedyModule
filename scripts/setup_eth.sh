f56b062aa97465c4c2b83b53f9e03794f2e36c86
Lukas Mazl <Lukas Mazl>
2020-10-17 12:55:06 +0200

    Updated setup_eth.sh script. The script can be launced with parameters from command line

--- scripts/setup_eth.sh
+++ scripts/setup_eth.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-for i in $(seq 1 750);
+re='^[0-9]+$'
+ipv4_re='^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-3]*[0-9]$'
+interface_name_prefix="eth-test-"
+ip="192.168.50.5/24"
+
+if ! [[ $1 =~ $re ]] ; then
+   echo "error: First parameter is null or is not number!"; exit 1
+fi
+
+if [[ -n $2 ]] ; then
+    echo "Setting interface prefix to '$2'"
+    interface_name_prefix=$2
+fi
+
+if [[ -n $3 ]] ; then
+    if ! [[ $3 =~ $ipv4_re ]] ; then
+      echo "error: Given IPv4 address '$3' is not valid"; exit 1
+    fi
+    echo "Default ip for interfaces is '$3'"
+    ip=$3
+fi
+
+for i in $(seq 1 $1);
 do
-  ip link add eth-test-$i type dummy
-  ip addr add 192.168.50.5/24 dev eth-test-$i
-  ip link set eth-test-$i up
+  interface_name=$interface_name_prefix$i
+  ip link add $interface_name type dummy
+  ip addr add $ip dev $interface_name
+  ip link set $interface_name up
 done;
