f8a8e60ca686cda67abb37b32c8b962939f8bf78
Lukas Mazl <Lukas Mazl>
2020-10-17 11:52:59 +0200

    Removed smoke test after module instalation

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,3 @@
 from distutils.core import setup, Extension
 setup(name='my_module', version='1.0', \
       ext_modules=[Extension('my_module', ['my_module.c'])])
-
-import my_module
-
-
-my_module.show_if()
-devices = my_module.get_all_if()
-print(devices)
-for device in devices:
-      print(device["name"])
-print("\n")
