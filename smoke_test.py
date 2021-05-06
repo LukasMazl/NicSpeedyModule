b6cbcac8e18ecc557c1a4bb68d5ab380cb97e07c
Lukas Mazl <Lukas Mazl>
2021-05-06 09:54:58 +0200

    Changed pytest for travis

--- smoke_test.py
+++ smoke_test.py
@@ -1,35 +1,35 @@
 import pytest
-import my_module
+import nicSpeedyModule
 import socket
 
 import netifaces
 
 
 def test_get_all_if_by_AF():
-    result = my_module.get_all_if_by_AF(socket.AF_PACKET)
+    result = nicSpeedyModule.get_all_if_by_AF(socket.AF_PACKET)
     netifaces_result = netifaces.interfaces()
     assert len(result) == len(netifaces_result)
 
 
 def test_get_ip():
-    result = my_module.get_ip('lo')
+    result = nicSpeedyModule.get_ip('lo')
     netifaces_result = netifaces.ifaddresses('lo')
     assert result == netifaces_result[2][0]['addr']
 
 def test_get_all_ipv4_if():
-    print(my_module.get_all_ipv4_if())
+    print(nicSpeedyModule.get_all_ipv4_if())
 
 def test_get_info_if_by_AF():
-    print(my_module.get_info_if_by_AF(socket.AF_INET))
+    print(nicSpeedyModule.get_info_if_by_AF(socket.AF_INET))
 
 def test_get_all_ipv6_if():
-    print(my_module.get_all_ipv6_if())
+    print(nicSpeedyModule.get_all_ipv6_if())
 
 def test_get_all_system_if():
-    print(my_module.get_all_system_if())
+    print(nicSpeedyModule.get_all_system_if())
 
 def test_get_info_about_active_devices():
-    print(my_module.get_info_about_active_devices())
+    print(nicSpeedyModule.get_info_about_active_devices())
 
 def test_get_active_devices():
-    print(my_module.get_active_devices())
+    print(nicSpeedyModule.get_active_devices())
